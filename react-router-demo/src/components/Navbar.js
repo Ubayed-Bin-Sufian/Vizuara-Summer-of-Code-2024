e45aa237aa4ff219a506699738bcbc90c084a1b3
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-21 12:06:10 +0600

    navLinkStyles added

--- react-router-demo/src/components/Navbar.js
+++ react-router-demo/src/components/Navbar.js
@@ -1,10 +1,22 @@
 import { NavLink } from "react-router-dom"  // Navlink is a component which understands whether a component is active or not 
 
 export const Navbar = () => {
+
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline',
+        }
+    }
+
     return (
         <nav>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/about'>About</NavLink>
+            <NavLink style={navLinkStyles} to='/'>
+                Home
+            </NavLink>
+            <NavLink style={navLinkStyles} to='/about'>
+                About
+            </NavLink>
         </nav>
     )
 }
