ba56d12a9775d640bc01a6d8ac27543636019755
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-22 17:12:16 +0600

    The user doesn't need to go back to the previous page

--- react-router-demo/src/components/Home.js
+++ react-router-demo/src/components/Home.js
@@ -5,7 +5,18 @@ export const Home = () => {
     return (
         <>
             <div>Home Page</div>
-            <button onClick={() => navigate('order-summary')}>Place Order</button>
+            <button onClick={() => navigate('order-summary', { replace: true })}>Place Order</button>
         </>
     )
 }
+
+/*  
+Using replace: true in the navigate function is useful when you want to navigate to a new route but do not want the user to be able 
+to return to the previous route using the back button.
+Examples:
+1. Order Confirmation Page
+2. User Logout
+3. Form Submission Success Page
+4. Account Deletion Confirmation
+5. Password Reset Completion
+*/
