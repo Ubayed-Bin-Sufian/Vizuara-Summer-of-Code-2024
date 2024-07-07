45fda774fb2b94a2c237d579bfc7c5ed4cc19613
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-07-07 10:52:24 +0600

    App component updated

--- coursera-react-app/src/App.js
+++ coursera-react-app/src/App.js
@@ -1,15 +1,8 @@
-// import Heading from "./components/Heading";
-
-function Heading(){
-  return (
-    <h1>This is an h1 heading.</h1>
-  )
-}
+import Heading from "./components/Heading";
 
 function App() {
   return (
-    <div className='App'>
-      This is the starting code for "Your first component" ungraded lab
+    <div className="App">
       <Heading />
     </div>
   );
