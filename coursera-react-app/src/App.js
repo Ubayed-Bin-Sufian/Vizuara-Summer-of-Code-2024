401746b1b39088751ea9044a23b2962faf1ea542
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-07-07 18:27:54 +0600

    components imported and props added in the App.js

--- coursera-react-app/src/App.js
+++ coursera-react-app/src/App.js
@@ -1,10 +1,15 @@
 import Heading from "./components/Heading";
+import Main from "./components/Main"
+import Sidebar from "./components/Sidebar"
+import './App.css'
 
 function App(props) {
   return (
-    <div className="App">
+    <div>
       <h1>{props.title}</h1>
-      <Heading />
+      <Heading name='Ubayed' color='green' />
+      <Main greet='Assalamu Alaikum' />
+      <Sidebar greeting='Ahlan wa Sahlan' />
     </div>
   );
 }
