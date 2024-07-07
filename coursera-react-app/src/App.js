b8571caef53a2e6d272140df43ebd75d70208572
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-07-07 18:45:48 +0600

    props added to pass to children components

--- coursera-react-app/src/App.js
+++ coursera-react-app/src/App.js
@@ -1,17 +1,12 @@
 import Heading from "./components/Heading";
-import Main from "./components/Main"
-import Sidebar from "./components/Sidebar"
-import './App.css'
 
-function App(props) {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      <Heading name='Ubayed' color='green' />
-      <Main greet='Assalamu Alaikum' />
-      <Sidebar greeting='Ahlan wa Sahlan' />
-    </div>
-  );
+function App() {
+  return (
+    <div className="App">
+      <Heading firstName="Bob" />
+      <Heading firstName="Any name other than Bob" />
+    </div>
+  );
 }
 
 export default App;
